a941034736a89620a9ebdffa1c6b0c5517895be6
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-07-21 22:12:35 +0300

    Initial Commit

---
+++ My_City_News/MVVM/Model/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_City_News.MVVM.Model
+{
+    class Model
+    {
+        // now cleared
+    }
+}
---
+++ My_City_News/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_City_News.MVVM.ViewModel
+{
+    class HomeViewModel
+    {
+    }
+}
