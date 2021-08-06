7d699b245f999383917c620af2d61a513ce41c72
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-08-07 02:12:29 +0300

    Add files via upload

---
+++ My_City_News/MVVM/Model/Custom Element/View/NewsControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using _5692comuaParser.Model.Custom_Element.ViewModel;
+using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace _5692comuaParser.Model.Custom_Element
+{
+    /// <summary>
+    /// Логика взаимодействия для NewsControl.xaml
+    /// </summary>
+    public partial class NewsControl : UserControl
+    {
+        public NewsControl(string categoryString, string dateTimeString, string headerString, string bodyString, string imagePath, string linkString)
+        {
+            InitializeComponent();
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(imagePath, $"{MainLogic.folderName}\\{++MainLogic.count}.jpg");
+            }
+
+            /*ImageSource использует агрессивное кеширование при загрузке фотографии, что приводило к невозможности
+            их удаления (файл занимался другим процессом). Оказывается, что в качестве источника фотографии отлично подойдёт
+            массив байтов, который сейчас и используется.*/
+
+            //ImageSource imageSource = null;
+            //try
+            //{
+            //    imageSource = new ImageSourceConverter().ConvertFromString($"{MainLogic.folderName}\\{MainLogic.count}.jpg") as ImageSource;
+            //}
+            //catch (System.Exception e) { }
+
+            this.DataContext = new NewsViewModel()
+            {
+                CategoryString = categoryString,
+                DateTimeString = dateTimeString,
+                HeaderString = headerString,
+                BodyString = bodyString,
+                ImagePath = File.ReadAllBytes($"{MainLogic.folderName}\\{MainLogic.count}.jpg"),
+                LinkString = linkString
+            };
+        }
+
+        private void headerTextBlock_MouseEnter(object sender, MouseEventArgs e) => (sender as TextBlock).TextDecorations = TextDecorations.Underline;
+        private void headerTextBlock_MouseLeave(object sender, MouseEventArgs e) => (sender as TextBlock).TextDecorations = null;
+    }
+}
