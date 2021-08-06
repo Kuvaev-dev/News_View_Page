a4617d8c746eef6e8948c6f0ae86b041e0015d2f
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-08-06 04:28:05 +0300

    Update DiscoveryView.xaml.cs

--- My_City_News/MVVM/View/DiscoveryView.xaml.cs
+++ My_City_News/MVVM/View/DiscoveryView.xaml.cs
@@ -22,68 +22,11 @@ namespace My_City_News.MVVM.View
     /// </summary>
     public partial class DiscovryView : UserControl
     {
-        public DiscovryView()
+        private DiscoveryViewModel dViewModel;
+        public DiscovryView(string headerString, byte[] imagePath, string linkString)
         {
             InitializeComponent();
-
-        }
-
-        WebRequest request = WebRequest.Create("https://www.5692.com.ua/news");
-
-        public void FillParams()
-        {
-            WebClient client = new WebClient();
-            string content = client.DownloadString("https://www.5692.com.ua/news");
-            client.Dispose();
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(@"/html/body/main/div[4]/div[2]/div[1]/div[1]/div/div[2]/a");
-
-            foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//text()"))
-            {
-                new1.Content=node.InnerText;
-            }
-        }
-
-        public void FillParamsS()
-        {
-            WebClient client = new WebClient();
-            string content = client.DownloadString("https://www.5692.com.ua/news");
-            client.Dispose();
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(@"/html/body/main/div[4]/div[2]/div[1]/div[2]/div/div[2]/a");
-
-            foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//text()"))
-            {
-                new2.Content = node.InnerText;
-            }
-        }
-
-        public void FillParamsT()
-        {
-            WebClient client = new WebClient();
-            string content = client.DownloadString("https://www.5692.com.ua/news");
-            client.Dispose();
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(@"/html/body/main/div[4]/div[2]/div[1]/div[3]/div/div[2]/a");
-
-            foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//text()"))
-            {
-                new3.Content = node.InnerText;
-            }
-        }
-
-        public void FillParamsF()
-        {
-            WebClient client = new WebClient();
-            string content = client.DownloadString("https://www.5692.com.ua/news");
-            client.Dispose();
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(@"/html/body/main/div[4]/div[2]/div[1]/div[4]/div/div[2]/a");
-
-            foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//text()"))
-            {
-                new4.Content = node.InnerText;
-            }
+            this.DataContext = dViewModel = new DiscoveryViewModel(headerString, imagePath, linkString);
         }
     }
 }
