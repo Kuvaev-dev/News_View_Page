7db187f3358834e6ee03a845e59d150da2f6d8ac
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-08-11 14:36:25 +0300

    Update Model.cs

--- My_City_News/MVVM/Model/Model.cs
+++ My_City_News/MVVM/Model/Model.cs
@@ -1,22 +1,28 @@
-﻿using HtmlAgilityPack;
+using HtmlAgilityPack;
+using My_City_News.Model.Custom_Element;
 using My_City_News.MVVM.Model.Core;
 using My_City_News.MVVM.ViewModel;
-using My_City_News.MVVM.Model.Custom_Element;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Text;
-using static System.Net.Mime.MediaTypeNames;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace My_City_News.MVVM.Model
 {
-    public class Model
+    class Model
     {
         public RelayCommand HomeViewCommand { get; set; }
         public RelayCommand DiscoveryViewCommand { get; set; }
         public HomeViewModel HomeVM { get; set; }
         public DiscoveryViewModel DiscoveryVM { get; set; }
         private object _currentView;
-        
+
         public readonly static string folderName = "pictures";
         public static int count = 0;
         public static void ParseSite(ref StackPanel newsStackPanel)
@@ -35,28 +41,29 @@ namespace My_City_News.MVVM.Model
             document.LoadHtml(content);
 
             List<HtmlNode> newsNodes = document.DocumentNode.SelectNodes("//div[@class=\'c-news-block\']").ToList();
-            UIElementCollection newsCollection = newsStackPanel.Children;         
+            UIElementCollection newsCollection = newsStackPanel.Children;
 
             Parallel.For(0, newsNodes.Count, i =>
             {
-                Application.Current.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, (ThreadStart)delegate() 
-                    {
-                        newsCollection.Add(new NewsControl
-                            (
-                                newsNodes[i].SelectNodes("//div[contains(@class, \'c-card-label\') and contains(@class, \'c-card-label--in-news\')]")[i].InnerText,
-                                newsNodes[i].SelectNodes("//span[@class=\'c-article-info__when\']/span")[i].InnerText,
-                                newsNodes[i].SelectNodes("//a[@class=\'c-news-block__title\']")[i].InnerText.Replace("&quot;", "\""),
-                                newsNodes[i].SelectNodes("//div[@class=\'c-news-block__text\']")[i].InnerText.Replace("&quot;", "\""),
-                                newsNodes[i].SelectNodes("//a[contains(@class, \'c-news-block__image\') and contains(@class, \'lazy-bg\')]")[i].Attributes["data-src"].Value,
-                                newsNodes[i].SelectNodes("//a[@class=\'c-news-block__title\']")[i].Attributes["href"].Value
-                            )
-                        );
-                   });
+                System.Windows.Application.Current.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, (ThreadStart)delegate ()
+                {
+                    newsCollection.Add(new NewsControl
+                        (
+                            newsNodes[i].SelectNodes("//div[contains(@class, \'c-card-label\') and contains(@class, \'c-card-label--in-news\')]")[i].InnerText,
+                            newsNodes[i].SelectNodes("//span[@class=\'c-article-info__when\']/span")[i].InnerText,
+                            newsNodes[i].SelectNodes("//a[@class=\'c-news-block__title\']")[i].InnerText.Replace("&quot;", "\""),
+                            newsNodes[i].SelectNodes("//div[@class=\'c-news-block__text\']")[i].InnerText.Replace("&quot;", "\""),
+                            newsNodes[i].SelectNodes("//a[contains(@class, \'c-news-block__image\') and contains(@class, \'lazy-bg\')]")[i].Attributes["data-src"].Value,
+                            newsNodes[i].SelectNodes("//a[@class=\'c-news-block__title\']")[i].Attributes["href"].Value
+                        )
+                    );
+                });
             });
 
             foreach (UIElement item in newsCollection)
             {
                 newsStackPanel.Children.Add(item);
             }
+        }
     }
 }
